af86ee2306a5dba7fd056e3952b5777ffd9bde39
gmsantos <gabriel.ms1@hotmail.com>
2020-01-06 22:12:40 -0300

    use dinamic envvar

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -2,14 +2,15 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Carter;
+using Microsoft.Extensions.Configuration;
 
 namespace CarterAPI.Modules
 {
     public class HomeModule : CarterModule
     {
-        public HomeModule()
+        public HomeModule(IConfiguration conf)
         {
-            Get("/", (req, res) => res.WriteAsync("Hello from Carter!"));
+            Get("/", (req, res) => res.WriteAsync("Hello from Carter! " + conf.GetValue<string>("Title")));
         }
     }
 }
